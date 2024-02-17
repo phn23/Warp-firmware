46a98846d8135c1d44cee863ab7bc4a09cb7e4b1
phn23 <78298829+phn23@users.noreply.github.com>
2024-02-17 00:25:46 +0000

    Update devSSD1331.c
    q7 update enable

--- src/boot/ksdk1.1.0/devSSD1331.c
+++ src/boot/ksdk1.1.0/devSSD1331.c
@@ -77,7 +77,7 @@ devSSD1331init(void)
 	PORT_HAL_SetMuxMode(PORTA_BASE, 8u, kPortMuxAlt3);
 	PORT_HAL_SetMuxMode(PORTA_BASE, 9u, kPortMuxAlt3);
 
-	enableSPIpins();
+	warpEnableSPIpins();
 
 	/*
 	 *	Override Warp firmware's use of these pins.
