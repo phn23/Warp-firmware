6f0a62fd92e9900e5bfe83bce96a7edb60821256
phn23 <78298829+phn23@users.noreply.github.com>
2024-02-17 21:25:31 +0000

    Update devSSD1331.c

--- src/boot/ksdk1.1.0/devSSD1331.c
+++ src/boot/ksdk1.1.0/devSSD1331.c
@@ -161,7 +161,8 @@ devSSD1331init(void)
 	 *	Any post-initialization drawing commands go here.
 	 */
 	//...
-
+	writeCommand(kSSD1331CommandMASTERCURRENT);
+	writeCommand(0x0F);
 
 	writeCommand(kSSD1331CommandCONTRASTA);  // R
 	writeCommand(0xFF);				
