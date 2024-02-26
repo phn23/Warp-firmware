dbbb258c311091238a868fae2db922c782dac5b6
phn23 <78298829+phn23@users.noreply.github.com>
2024-02-26 20:24:08 +0000

    Update config.h

--- src/boot/ksdk1.1.0/config.h
+++ src/boot/ksdk1.1.0/config.h
@@ -86,6 +86,7 @@
 #define WARP_BUILD_ENABLE_DEVSI4705   0
 #define WARP_BUILD_ENABLE_DEVSI7021   0
 #define WARP_BUILD_ENABLE_DEVTCS34725 0
+
 #elif (WARP_BUILD_ENABLE_FRDMKL03)
 #define WARP_BUILD_ENABLE_DEVADXL362  0
 #define WARP_BUILD_ENABLE_DEVAMG8834  0
@@ -104,10 +105,15 @@
 #define WARP_BUILD_ENABLE_DEVLPS25H   0
 #define WARP_BUILD_ENABLE_DEVMAG3110  0
 #define WARP_BUILD_ENABLE_DEVMMA8451Q 1
+
+// ADDED
+#define WARP_BUILD_ENABLE_DEVINA219 1
+
 #define WARP_BUILD_ENABLE_DEVRV8803C7 0
 #define WARP_BUILD_ENABLE_DEVSI4705   0
 #define WARP_BUILD_ENABLE_DEVSI7021   0
 #define WARP_BUILD_ENABLE_DEVTCS34725 0
+
 #else
 /*
  *	Otherwise: Edit these to set which code paths are activated in the firmware compilation
@@ -179,6 +185,9 @@ typedef enum
 	kWarpDefaultSupplyVoltageMillivoltsBMX055gyro  = 2600,
 	kWarpDefaultSupplyVoltageMillivoltsBMX055mag   = 2600,
 	kWarpDefaultSupplyVoltageMillivoltsMMA8451Q    = 1800,
+
+	kWarpDefaultSupplyVoltageMillivoltsINA219 = ,
+
 	kWarpDefaultSupplyVoltageMillivoltsLPS25H      = 1800,
 	kWarpDefaultSupplyVoltageMillivoltsHDC1000     = 1800,
 	kWarpDefaultSupplyVoltageMillivoltsMAG3110     = 1800,
