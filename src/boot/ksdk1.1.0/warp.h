d4e983ffd3b622878c7cf6c7ac310da7be6d1ff3
phn23 <78298829+phn23@users.noreply.github.com>
2024-02-27 18:24:17 +0000

    Update warp.h - removed ina219 due to defined adafruit lib

--- src/boot/ksdk1.1.0/warp.h
+++ src/boot/ksdk1.1.0/warp.h
@@ -114,9 +114,6 @@ typedef enum
 	kWarpSensorConfigurationRegisterMMA8451QF_SETUP			= 0x09,
 	kWarpSensorConfigurationRegisterMMA8451QCTRL_REG1		= 0x2A,
 
-	// ADDED
-	kWarpSensorOutputRegisterINA219CONFIG_REG			= 0x00,  
-
 	kWarpSensorConfigurationRegisterMAG3110CTRL_REG1		= 0x10,
 	kWarpSensorConfigurationRegisterMAG3110CTRL_REG2		= 0x11,
 
@@ -181,11 +178,6 @@ typedef enum
 	kWarpSensorOutputRegisterMMA8451QOUT_Z_MSB			= 0x05,
 	kWarpSensorOutputRegisterMMA8451QOUT_Z_LSB			= 0x06,
 
-	// ADDED
-	kWarpSensorOutputRegisterINA219OUT_SHUNT			= 0x01,
-	kWarpSensorOutputRegisterINA219OUT_BUS				= 0x02,
-	kWarpSensorOutputRegisterINA219OUT_POWER			= 0x03,
-	kWarpSensorOutputRegisterINA219OUT_CURRENT			= 0x04,
 
 
 
