8ba3cb4853264453b4629177837dc5f6e7b87b30
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-18 01:36:38 +0000

    Update my_algo.c

--- src/boot/ksdk1.1.0/my_algo.c
+++ src/boot/ksdk1.1.0/my_algo.c
@@ -1,1 +1,16 @@
+incldue <maths.h.
+// the algorithm is like this
 
+// Use Bayesian / or a simple logistic regresson
+
+// run for 1000 loops
+
+// determine whether the wheelchair is fallen
+
+bool logistic_regression_pred(){
+
+  return 0;
+
+  return 1;
+
+}
