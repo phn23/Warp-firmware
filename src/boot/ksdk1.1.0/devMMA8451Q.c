a3c6166962ea5f88040696f2172cc7d14f962c86
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-19 17:56:27 +0000

    Update devMMA8451Q.c - arctan

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -498,7 +498,7 @@ int normal_loop() {
 
         get_acceleration(&x_acceleration, &y_acceleration, &z_acceleration);
 
-        tilt_side = 4000;
+        tilt_side = abs(atan2(z_acceleration, y_acceleration) * 18000 / M_PI);
         if (tilt_side > threshold_tilt_angle) {
             trigger = 1;
         }
