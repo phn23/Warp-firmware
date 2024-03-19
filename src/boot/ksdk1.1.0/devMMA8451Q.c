8eb24a7fadd4d804b83e8b25c33285e26d1b4f9f
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-19 17:31:23 +0000

    Update devMMA8451Q.c -rmv

--- src/boot/ksdk1.1.0/devMMA8451Q.c
+++ src/boot/ksdk1.1.0/devMMA8451Q.c
@@ -508,7 +508,7 @@ int normal_loop(){
 		bool flip = false;
 
 		get_acceleration(&x_acceleration, &y_acceleration, &z_acceleration);
-		tilt_side = abs(atan2(z_acceleration, y_acceleration) * 18000 / M_PI);
+		// tilt_side = abs(atan2(z_acceleration, y_acceleration) * 18000 / M_PI);
 
 
 	/***************************************************
