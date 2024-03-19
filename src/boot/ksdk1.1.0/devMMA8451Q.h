c7331a9471506e85e668263a9c899def366616d8
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-19 12:34:44 +0000

    Update devMMA8451Q.h

--- src/boot/ksdk1.1.0/devMMA8451Q.h
+++ src/boot/ksdk1.1.0/devMMA8451Q.h
@@ -43,6 +43,14 @@ WarpStatus 	configureSensorMMA8451Q(uint8_t payloadF_SETUP, uint8_t payloadCTRL_
 void		printSensorDataMMA8451Q(bool hexModeFlag);
 uint8_t		appendSensorDataMMA8451Q(uint8_t* buf);
 
+
+
+bool tilt_angle_trigger();
+void get_acceleration(int16_t* x_acc, int16_t* y_acc, int16_t* z_acc);
+int normal_loop();
+
+#define threshold_tilt_angle 3000 // 3000 / 100 = 30 degrees
+
 const uint8_t bytesPerMeasurementMMA8451Q            = 6;
 const uint8_t bytesPerReadingMMA8451Q                = 2;
 const uint8_t numberOfReadingsPerMeasurementMMA8451Q = 3;
