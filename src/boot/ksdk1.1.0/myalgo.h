4c68ddb8f7e01c54962b55af1fb8454a438533e8
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-19 11:54:14 +0000

    Update myalgo.h

--- src/boot/ksdk1.1.0/myalgo.h
+++ src/boot/ksdk1.1.0/myalgo.h
@@ -1,3 +1,5 @@
 bool tilt_angle_trigger();
 void get_acceleration(int16_t* x_acc, int16_t* y_acc, int16_t* z_acc);
 int normal_loop();
+
+#define threshold_tilt_angle 3000 // 3000 / 100 = 30 degrees
