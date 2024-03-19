374d726297b7b9a9a75e7ada4b0ee22293e4c860
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-19 19:24:14 +0000

    Update devMMA8451Q.h

--- src/boot/ksdk1.1.0/devMMA8451Q.h
+++ src/boot/ksdk1.1.0/devMMA8451Q.h
@@ -49,7 +49,7 @@ bool tilt_angle_trigger();
 void get_acceleration(int16_t* x_acc, int16_t* y_acc, int16_t* z_acc);
 int normal_loop();
 
-#define threshold_tilt_angle 3000 // 3000 / 100 = 30 degrees
+#define threshold_anomaly 1000 //
 
 const uint8_t bytesPerMeasurementMMA8451Q            = 6;
 const uint8_t bytesPerReadingMMA8451Q                = 2;
