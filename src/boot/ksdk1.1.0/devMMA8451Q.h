a5310a82384c8a0a1e797b1e306ca481c0ddbd28
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-20 21:21:23 +0000

    Update devMMA8451Q.h

--- src/boot/ksdk1.1.0/devMMA8451Q.h
+++ src/boot/ksdk1.1.0/devMMA8451Q.h
@@ -45,7 +45,7 @@ uint8_t		appendSensorDataMMA8451Q(uint8_t* buf);
 
 
 
-bool tilt_angle_trigger();
+// bool tilt_angle_trigger();
 void get_acceleration(int16_t* x_acc, int16_t* y_acc, int16_t* z_acc);
 int normal_loop();
 
