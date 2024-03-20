084a39e4ef6f913c5ce5061db6fb312d1036c0e3
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-20 23:40:33 +0000

    Update boot.c

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -1981,19 +1981,19 @@ main(void)
 		OSA_TimeDelay(100);
 
 
-		int16_t x_acceleration_test;
-		int16_t y_acceleration_test;
-		int16_t z_acceleration_test;
+		// int16_t x_acceleration_test;
+		// int16_t y_acceleration_test;
+		// int16_t z_acceleration_test;
 
-		get_acceleration(&x_acceleration_test, &y_acceleration_test, &z_acceleration_test);
-		warpPrint("%d, %d, %d \n", x_acceleration_test, y_acceleration_test, z_acceleration_test);
+		// get_acceleration(&x_acceleration_test, &y_acceleration_test, &z_acceleration_test);
+		// warpPrint("%d, %d, %d \n", x_acceleration_test, y_acceleration_test, z_acceleration_test);
 
 		
-		OSA_TimeDelay(100);
-		get_acceleration(&x_acceleration_test, &y_acceleration_test, &z_acceleration_test);
-		warpPrint("%d, %d, %d \n", x_acceleration_test, y_acceleration_test, z_acceleration_test);
+		// OSA_TimeDelay(100);
+		// get_acceleration(&x_acceleration_test, &y_acceleration_test, &z_acceleration_test);
+		// warpPrint("%d, %d, %d \n", x_acceleration_test, y_acceleration_test, z_acceleration_test);
 
-		warpPrint("big_loop start \n");
+		// warpPrint("big_loop start \n");
 		// bool performance = normal_loop();
 		
 		// warpPrint("normal loop ends and the classification flag is obtained \n");
