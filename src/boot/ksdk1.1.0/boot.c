028e2c272caa101496f58b857183b52ecc53e128
phn23 <78298829+phn23@users.noreply.github.com>
2024-03-20 16:38:01 +0000

    Update boot.c

--- src/boot/ksdk1.1.0/boot.c
+++ src/boot/ksdk1.1.0/boot.c
@@ -1976,17 +1976,18 @@ main(void)
 		while (OSA_TimeGetMsec() - timeAtStart_ssd < 5000){
 			get_acceleration(&x_acceleration_test, &y_acceleration_test, &z_acceleration_test);
 		// 	readSensorRegisterMMA8451Q(0x01, 6);
-		// 	
-		for (int i = 0; i<100; i++){
-			// x_acc_test = MMA8541Q_get_acceleration_x();
+		//
+		// }	
+		// for (int i = 0; i<100; i++){
+		// 	// x_acc_test = MMA8541Q_get_acceleration_x();
 
-			// this should print all 3 combined values;
-			// 0 means decimal
-			printSensorDataMMA8451Q(0);
+		// 	// this should print all 3 combined values;
+		// 	// 0 means decimal
+		// 	printSensorDataMMA8451Q(0);
 			
-			// warpPrint("%d \n", x_acc_test);
-			warpPrint("\n");
-		}
+		// 	// warpPrint("%d \n", x_acc_test);
+		// 	warpPrint("\n");
+		// }
 		// warpPrint("test loop done\n");
 
 		
